759e7709f82726c144b344d03f10a59bd9b34806
indianfootballstatistics <indianfootballstatistics@gmail.com>
2020-06-21 01:23:02 +0530

    Property Component addition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AdminComponent} from './components/admin/admin.component'
+import {PropertyComponent} from './components/property/property.component'
  
 const routes: Routes = [
   { path: '', redirectTo: 'Admin', pathMatch: 'full'},
-  { path: 'Admin', component: AdminComponent }
+  { path: 'Admin', component: AdminComponent },
+  { path: 'ListProperty', component: PropertyComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { PropertyComponent } from './components/property/property.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AdminComponent } from './components/admin/admin.component';
     HeaderComponent,
     FooterComponent,
     AdminComponent,
+    PropertyComponent,
   ],
   imports: [
     BrowserModule,
