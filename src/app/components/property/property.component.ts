ef8f297dec17b782a07d00c68f4c3f00fb705d9c
indianfootballstatistics <indianfootballstatistics@gmail.com>
2020-06-23 00:59:24 +0530

    Basic UI structure for adding properties

--- src/app/components/property/property.component.ts
+++ src/app/components/property/property.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    
+  }
+  urls = [];
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+        var filesAmount = event.target.files.length;
+        for (let i = 0; i < filesAmount; i++) {
+                var reader = new FileReader();
+
+                reader.onload = (event:any) => {
+                  console.log(event.target.result);
+                   this.urls.push(event.target.result); 
+                }
+                console.log(this.urls);
+
+                reader.readAsDataURL(event.target.files[i]);
+        }
+    }
   }
 
 }
